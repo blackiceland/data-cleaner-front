1afea35d7de358344b1ab882dde6d4df957922b5
blackiceland <jaggervid1@gmail.com>
2025-06-23 22:41:51 +0600

    Новая аутентификация 4.

--- script/src/Code.js
+++ script/src/Code.js
@@ -1,29 +1,30 @@
 function onOpen() {
-  SpreadsheetApp.getUi()
-      .createMenu('DataCleaner')
-      .addItem('Open', 'openDataCleaner')
-      .addToUi();
+    SpreadsheetApp.getUi()
+        .createMenu('DataCleaner')
+        .addItem('Open', 'openDataCleaner')
+        .addToUi();
 }
 
 function openDataCleaner() {
-  const html = HtmlService
-      .createHtmlOutputFromFile('dialog')
-      .setSandboxMode(HtmlService.SandboxMode.IFRAME)
-      .setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL)
-      .setWidth(400)
-      .setHeight(300);
+    const html = HtmlService
+        .createTemplateFromFile('dialog')
+        .evaluate()
+        .setSandboxMode(HtmlService.SandboxMode.IFRAME)
+        .setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL)
+        .setWidth(400)
+        .setHeight(300);
 
-  SpreadsheetApp.getUi().showModelessDialog(html, 'DataCleaner');
+    SpreadsheetApp.getUi().showModelessDialog(html, 'DataCleaner');
 }
 
 function getActiveRangeValues() {
-  return SpreadsheetApp.getActiveRange().getValues();
+    return SpreadsheetApp.getActiveRange().getValues();
 }
 
 function highlightExactRows(rowIndexes) {
-  if (!rowIndexes || rowIndexes.length === 0) return;
-  const sheet = SpreadsheetApp.getActiveSheet();
-  const lastCol = sheet.getLastColumn();
-  const ranges = rowIndexes.map(i => sheet.getRange(i + 1, 1, 1, lastCol).getA1Notation());
-  sheet.getRangeList(ranges).setBackground('#F28B82');
+    if (!rowIndexes || rowIndexes.length === 0) return;
+    const sheet = SpreadsheetApp.getActiveSheet();
+    const lastCol = sheet.getLastColumn();
+    const ranges = rowIndexes.map(i => sheet.getRange(i + 1, 1, 1, lastCol).getA1Notation());
+    sheet.getRangeList(ranges).setBackground('#F28B82');
 }
