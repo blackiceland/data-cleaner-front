42a2e08dadd530d7b226e3821b9c12194d65a9d1
blackiceland <jaggervid1@gmail.com>
2025-07-02 20:43:42 +0600

    Highlights candidates 2.

--- script/src/Code.js
+++ script/src/Code.js
@@ -1,54 +1,77 @@
 function onOpen() {
-  SpreadsheetApp.getUi()
-      .createMenu('DataCleaner')
-      .addItem('Open', 'openDataCleaner')
-      .addToUi();
+    SpreadsheetApp.getUi()
+        .createMenu('DataCleaner')
+        .addItem('Open', 'openDataCleaner')
+        .addToUi();
 }
 
 function openDataCleaner() {
-  const html = HtmlService
-      .createTemplateFromFile('dialog')
-      .evaluate()
-      .setSandboxMode(HtmlService.SandboxMode.IFRAME)
-      .setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL)
-      .setWidth(600)
-      .setHeight(560);
-  SpreadsheetApp.getUi().showModelessDialog(html, 'DataCleaner');
+    const html = HtmlService
+        .createTemplateFromFile('dialog')
+        .evaluate()
+        .setSandboxMode(HtmlService.SandboxMode.IFRAME)
+        .setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL)
+        .setWidth(600)
+        .setHeight(560);
+    SpreadsheetApp.getUi().showModelessDialog(html, 'DataCleaner');
 }
 
 function getIdToken() {
-  return ScriptApp.getOAuthToken();
+    return ScriptApp.getOAuthToken();
 }
 
 function getActiveRangeValues() {
-  return SpreadsheetApp.getActiveRange().getValues();
+    return SpreadsheetApp.getActiveRange().getValues();
 }
 
 function highlightExactRows(rowIdxArr) {
-  if (!rowIdxArr || !rowIdxArr.length) return;
-  const sheet = SpreadsheetApp.getActiveSheet();
-  const width = sheet.getLastColumn();
-  rowIdxArr.forEach(i =>
-      sheet.getRange(i + 1, 1, 1, width).setBackground('#F28B82'));
+    if (!rowIdxArr || !rowIdxArr.length) return;
+    const sheet = SpreadsheetApp.getActiveSheet();
+    const width = sheet.getLastColumn();
+    rowIdxArr.forEach(i =>
+        sheet.getRange(i + 1, 1, 1, width).setBackground('#F28B82'));
 }
 
 function getActiveRangeA1() {
-  return SpreadsheetApp.getActiveRange().getA1Notation();
+    return SpreadsheetApp.getActiveRange().getA1Notation();
 }
 
 function getInitialRangeA1() {
-  const sel = SpreadsheetApp.getActiveRange();
-  if (sel.getNumRows() > 1 || sel.getNumColumns() > 1) {
-    return sel.getA1Notation();
-  }
-  return SpreadsheetApp.getActiveSheet().getDataRange().getA1Notation();
+    const sel = SpreadsheetApp.getActiveRange();
+    if (sel.getNumRows() > 1 || sel.getNumColumns() > 1) {
+        return sel.getA1Notation();
+    }
+    return SpreadsheetApp.getActiveSheet().getDataRange().getA1Notation();
 }
 
 function highlightPossibleRows(rowIdxArr) {
-  if (!rowIdxArr || !rowIdxArr.length) return;
-  const sheet = SpreadsheetApp.getActiveSheet();
-  const width = sheet.getLastColumn();
-  rowIdxArr.forEach(i =>
-      sheet.getRange(i + 1, 1, 1, width).setBackground('#FBBC04'));
+    if (!rowIdxArr || !rowIdxArr.length) return;
+    const sheet = SpreadsheetApp.getActiveSheet();
+    const width = sheet.getLastColumn();
+    rowIdxArr.forEach(i =>
+        sheet.getRange(i + 1, 1, 1, width).setBackground('#FBBC04'));
 }
+
+function highlightCanonRows(rowIdxArr) {
+    if (!rowIdxArr || !rowIdxArr.length) return;
+    const sh = SpreadsheetApp.getActiveSheet();
+    const cols = sh.getLastColumn();
+    rowIdxArr.forEach(i => {
+        const r = sh.getRange(i + 1, 1, 1, cols);
+        r.setBackground('#FFFBEA')
+            .setBorder(true, false, false, false, false, false,
+                '#F28B82',
+                SpreadsheetApp.BorderStyle.SOLID_MEDIUM);
+    });
+}
+
+function highlightRows(rowIdxArr, color) {
+    if (!rowIdxArr || !rowIdxArr.length) return;
+    const sheet = SpreadsheetApp.getActiveSheet();
+    const width = sheet.getLastColumn();
+    rowIdxArr.forEach(i =>
+        sheet.getRange(i + 1, 1, 1, width).setBackground(color));
+}
+
+
 
